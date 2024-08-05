ef8e0cf85ccbe962f8e1dbd3e0789dbf21bc9a89
Jamie Gardner <jamie@tgo.dev>
2024-08-05 09:51:16 +0100

    Add pre title

--- vite/js/Components/Atoms/Pre.vue
+++ vite/js/Components/Atoms/Pre.vue
@@ -1,3 +1,15 @@
+<script setup>
+defineProps({
+    title: String,
+});
+</script>
+
 <template>
-<pre class="mb-1 overflow-x-auto rounded-md bg-white p-2 text-sm"><slot/></pre>
+    <div>
+        <h2 v-if="title" class="my-1 text-sm font-medium text-gray-400">
+            {{ title }}
+        </h2>
+        <pre
+            class="mb-1 overflow-x-auto rounded-md bg-white p-2 text-sm"><slot /></pre>
+    </div>
 </template>
