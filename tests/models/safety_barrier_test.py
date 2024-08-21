7061edc66a5f3ceec53806e8d3fc9ea16382f288
Jamie Gardner <jamie@tgo.dev>
2024-08-21 09:46:06 +0100

    wip: additional tests

--- tests/models/safety_barrier_test.py
+++ tests/models/safety_barrier_test.py
@@ -2,38 +2,57 @@ from app.models.safety_barrier import SafetyBarrier
 from tests import sample_data
 
 
-def test_it_returns_correctly_for_discrete_time_linear_safety_barrier_certificates(sample_data):
-    """
-    The Lyapunov function to return is then
-    $\\mathcal{V}(x) = x^\\top Px$ and the controller to return is $u=\\mathcal{U}_{0,T}HP^{-1}x$.
-    """
-    sample_data['mode'] = 'Safety'
-    sample_data['model'] = 'Linear'
-    sample_data['timing'] = 'Discrete-Time'
-    safety = SafetyBarrier(data=sample_data)
+# --- Discrete-Time Linear System Barrier ---
 
-    actual = safety.calculate()
+def test_it_returns_the_correct_barrier_expression(sample_data):
+    actual = _discrete_setup(sample_data)
 
     assert actual['barrier']['expression'] == 'x^T @ P @ x'
+
+
+def test_it_contains_the_barrier_value(sample_data):
+    actual = _discrete_setup(sample_data)
+
     assert 'P' in actual['barrier']['values']
+
+
+def test_it_contains_a_valid_barrier_value(sample_data):
+    actual = _discrete_setup(sample_data)
+
     assert actual['barrier']['values']['P'] is not None
 
+
+def test_it_returns_the_correct_controller_expression(sample_data):
+    actual = _discrete_setup(sample_data)
+
     assert actual['controller']['expression'] == 'U_{0,T} @ H @ P @ x'
+
+
+def test_it_returns_the_correct_controller_values(sample_data):
+    actual = _discrete_setup(sample_data)
+
     assert 'U' in actual['controller']['values']
     assert actual['controller']['values']['U'] is not None
     assert 'H' in actual['controller']['values']
     assert actual['controller']['values']['H'] is not None
 
+
+def test_it_calculates_the_level_sets(sample_data):
+    actual = _discrete_setup(sample_data)
+
     assert actual['gamma'] is not None
     assert actual['lambda'] is not None
+
+
+def test_it_returns_valid_level_sets(sample_data):
+    actual = _discrete_setup(sample_data)
+
     assert actual['gamma'] < actual['lambda']
 
 
+# --- Continuous-Time Linear System Barrier ---
+
 def test_it_returns_correctly_for_continuous_time_linear_safety_barrier_certificates(sample_data):
-    """
-    The Lyapunov function to return is then
-    $\\mathcal{V}(x) = x^\\top Px$ and the controller to return is $u=\\mathcal{U}_{0,T}HP^{-1}x$.
-    """
     sample_data['mode'] = 'Safety'
     sample_data['model'] = 'Linear'
     sample_data['timing'] = 'Continuous-Time'
@@ -51,3 +70,39 @@ def test_it_returns_correctly_for_continuous_time_linear_safety_barrier_certific
     assert actual['gamma'] is not None
     assert actual['lambda'] is not None
     assert actual['gamma'] < actual['lambda']
+
+
+def _discrete_linear_setup(sample_data):
+    sample_data['mode'] = 'Safety'
+    sample_data['timing'] = 'Discrete-Time'
+    sample_data['model'] = 'Linear'
+    safety = SafetyBarrier(data=sample_data)
+
+    return safety.calculate()
+
+
+def _continuous_linear_setup(sample_data):
+    sample_data['mode'] = 'Safety'
+    sample_data['timing'] = 'Continuous-Time'
+    sample_data['model'] = 'Linear'
+    safety = SafetyBarrier(data=sample_data)
+
+    return safety.calculate()
+
+
+def _discrete_polynomial_setup(sample_data):
+    sample_data['mode'] = 'Safety'
+    sample_data['timing'] = 'Discrete-Time'
+    sample_data['model'] = 'Polynomial'
+    safety = SafetyBarrier(data=sample_data)
+
+    return safety.calculate()
+
+
+def _continuous_polynomial_setup(sample_data):
+    sample_data['mode'] = 'Safety'
+    sample_data['timing'] = 'Continuous-Time'
+    sample_data['model'] = 'Polynomial'
+    safety = SafetyBarrier(data=sample_data)
+
+    return safety.calculate()
