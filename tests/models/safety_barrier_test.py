b5376d3e800cd67bf6519ea0af000c67d7a21d34
Jamie Gardner <jamie@tgo.dev>
2024-08-27 10:24:50 +0100

    Refactor tests to set up sample data

--- tests/models/safety_barrier_test.py
+++ tests/models/safety_barrier_test.py
@@ -39,31 +39,41 @@ def test_it_calculates_a_barrier_for_discrete_time_linear_system(sample_data):
 
 
 def test_it_returns_the_correct_barrier_expression(sample_data):
-    actual = _discrete_setup(sample_data)
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
 
     assert actual['barrier']['expression'] == 'x^T @ P @ x'
 
 
 def test_it_contains_the_barrier_value(sample_data):
-    actual = _discrete_setup(sample_data)
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
 
     assert 'P' in actual['barrier']['values']
 
 
 def test_it_contains_a_valid_barrier_value(sample_data):
-    actual = _discrete_setup(sample_data)
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
 
     assert actual['barrier']['values']['P'] is not None
 
 
 def test_it_returns_the_correct_controller_expression(sample_data):
-    actual = _discrete_setup(sample_data)
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
 
     assert actual['controller']['expression'] == 'U_{0,T} @ H @ P @ x'
 
 
 def test_it_returns_the_correct_controller_values(sample_data):
-    actual = _discrete_setup(sample_data)
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
 
     assert 'U' in actual['controller']['values']
     assert actual['controller']['values']['U'] is not None
@@ -72,16 +82,37 @@ def test_it_returns_the_correct_controller_values(sample_data):
 
 
 def test_it_calculates_the_level_sets(sample_data):
-    actual = _discrete_setup(sample_data)
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
 
     assert actual['gamma'] is not None
     assert actual['lambda'] is not None
 
 
 def test_it_returns_valid_level_sets(sample_data):
-    actual = _discrete_setup(sample_data)
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
 
     assert actual['gamma'] < actual['lambda']
+
+
+def test_it_returns_a_valid_barrier_for_the_level_sets(sample_data):
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
+
+    # Assert that B(x) <= gamma for all x in X_initial aka initial_state
+    # Assert that B(x) >= lambda for all x in X_unsafe aka unsafe_states
+
+
+def test_the_next_step_is_less_than_the_current_step(sample_data):
+    sample_data = _discrete_linear_setup(sample_data)
+
+    actual = SafetyBarrier(sample_data).calculate()
+
+    # Assert that B(x+) <= B(x)
 
 
 # --- Continuous-Time Linear System Barrier ---
@@ -110,33 +141,29 @@ def _discrete_linear_setup(sample_data):
     sample_data['mode'] = 'Safety'
     sample_data['timing'] = 'Discrete-Time'
     sample_data['model'] = 'Linear'
-    safety = SafetyBarrier(data=sample_data)
 
-    return safety.calculate()
+    return sample_data
 
 
 def _continuous_linear_setup(sample_data):
     sample_data['mode'] = 'Safety'
     sample_data['timing'] = 'Continuous-Time'
     sample_data['model'] = 'Linear'
-    safety = SafetyBarrier(data=sample_data)
 
-    return safety.calculate()
+    return sample_data
 
 
 def _discrete_polynomial_setup(sample_data):
     sample_data['mode'] = 'Safety'
     sample_data['timing'] = 'Discrete-Time'
     sample_data['model'] = 'Polynomial'
-    safety = SafetyBarrier(data=sample_data)
 
-    return safety.calculate()
+    return sample_data
 
 
 def _continuous_polynomial_setup(sample_data):
     sample_data['mode'] = 'Safety'
     sample_data['timing'] = 'Continuous-Time'
     sample_data['model'] = 'Polynomial'
-    safety = SafetyBarrier(data=sample_data)
 
-    return safety.calculate()
+    return sample_data
