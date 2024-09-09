4ee13526922507da694f9ec3fac5d7a3ae743058
Jamie Gardner <jamie@tgo.dev>
2024-09-09 10:54:28 +0100

    Copy output

--- vite/js/Components/Atoms/Pre.vue
+++ vite/js/Components/Atoms/Pre.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { onMounted, ref, useSlots } from "vue";
 import {
     ClipboardDocumentIcon,
     ClipboardDocumentCheckIcon,
 } from "@heroicons/vue/16/solid";
 import { useClipboard } from "@vueuse/core";
+import { ref, useSlots } from "vue";
 
 defineProps({
     title: String,
@@ -12,8 +12,13 @@ defineProps({
 
 const slots = useSlots();
 
-const content = ref();
-const { text, copy, copied, isSupported } = useClipboard({ content });
+const source = ref();
+
+const { text, copy, copied, isSupported } = useClipboard({ source });
+
+const copyEvent = () => {
+    copy(slots.default()[0].children);
+};
 </script>
 
 <template>
@@ -21,23 +26,14 @@ const { text, copy, copied, isSupported } = useClipboard({ content });
         <h2 v-if="title" class="my-1 text-sm text-gray-400">
             {{ title }}
         </h2>
-        <p class="text-white">
-            Supported: {{ isSupported }}
-        </p>
-        <p class="text-white">
-            Text: {{ text }}
-        </p>
-        <p class="text-white">
-            Content: {{ content }}
-        </p>
-        <p class="text-white">
-            Slot: {{ slots.default }}
-        </p>
         <pre
-            class="relative mb-1 overflow-x-auto rounded-md bg-white p-2 text-sm dark:bg-gray-900/50 dark:text-gray-100"><span
+            class="relative mb-1 overflow-x-auto whitespace-pre-line rounded-md bg-white p-2 text-sm dark:bg-gray-900/50 dark:text-gray-100"><span
             v-if="isSupported"
-            class="cursor-pointer absolute top-0 right-0 text-xs text-gray-400 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-200"
-            @click="copy(content)"><ClipboardDocumentIcon v-if="!copied" class="size-4" /><ClipboardDocumentCheckIcon
-            v-else class="size-6" /></span><slot ref="content" /></pre>
+            class="cursor-pointer absolute top-2 right-2 text-xs text-gray-400 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-200"
+            @click="copyEvent"
+        ><ClipboardDocumentIcon v-if="!copied" class="size-4"
+        /><ClipboardDocumentCheckIcon v-else class="size-4"
+        /></span><slot class="pointer-events-none" ref="content" />
+        </pre>
     </div>
 </template>
