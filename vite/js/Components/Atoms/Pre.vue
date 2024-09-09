e71a76322c04255e245bb7270b8d4663650c01c1
Jamie Gardner <jamie@tgo.dev>
2024-09-09 11:08:13 +0100

    Tweak overflow for output pre's

--- vite/js/Components/Atoms/Pre.vue
+++ vite/js/Components/Atoms/Pre.vue
@@ -22,18 +22,22 @@ const copyEvent = () => {
 </script>
 
 <template>
-    <div class="relative">
+    <div>
         <h2 v-if="title" class="my-1 text-sm text-gray-400">
             {{ title }}
         </h2>
-        <pre
-            class="relative mb-1 overflow-x-auto whitespace-pre-line rounded-md bg-white p-2 text-sm dark:bg-gray-900/50 dark:text-gray-100"><span
-            v-if="isSupported"
-            class="cursor-pointer absolute top-2 right-2 text-xs text-gray-400 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-200"
-            @click="copyEvent"
-        ><ClipboardDocumentIcon v-if="!copied" class="size-4"
-        /><ClipboardDocumentCheckIcon v-else class="size-4"
-        /></span><slot class="pointer-events-none" ref="content" />
-        </pre>
+        <div class="relative">
+            <pre
+                class="relative mb-1 max-h-36 overflow-auto whitespace-pre-line rounded-md bg-white p-2 text-sm dark:bg-gray-900/50 dark:text-gray-100"><slot
+                ref="content" class="pointer-events-none" />
+            </pre>
+            <span
+                v-if="isSupported"
+                class="absolute right-2.5 top-2.5 cursor-pointer text-xs text-gray-400 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-200"
+                @click="copyEvent">
+                <ClipboardDocumentIcon v-if="!copied" class="size-4" />
+                <ClipboardDocumentCheckIcon v-else class="size-4" />
+            </span>
+        </div>
     </div>
 </template>
