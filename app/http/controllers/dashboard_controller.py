8c659bffe7aaf63f99c298d43a9e23bba9c563a8
Jamie Gardner <jamie@tgo.dev>
2024-10-23 14:58:14 +0100

    Catch backend errors

--- app/http/controllers/dashboard_controller.py
+++ app/http/controllers/dashboard_controller.py
@@ -5,12 +5,14 @@ from typing import Union
 from flask import Blueprint, Response, jsonify, request
 from flask_inertia import lazy_include, render_inertia
 import sympy as sp
+from picos import SolutionFailure
 
 import tests
 from app.models.safety_barrier import SafetyBarrier
 from app.models.stability import Stability
 
-bp = Blueprint('dashboard', __name__)
+bp = Blueprint("dashboard", __name__)
+
 
 def calculate_result() -> dict:
     """
@@ -26,23 +28,31 @@ def calculate_result() -> dict:
 
     start_time = time()
 
-    if data['mode'] == 'Stability':
-        function_name = 'stability_function'
-        stability_function = Stability().create(data).calculate()
-    else:
-        function_name = 'barrier_function'
-        barrier_function = SafetyBarrier(data).calculate()
+    try:
+        if data["mode"] == "Stability":
+            results = Stability().create(data).calculate()
+        else:
+            results = SafetyBarrier(data).calculate()
+    except SolutionFailure as e:
+        results = {
+            "error": "Solution Failure",
+            "description": str(e),
+        }
+    except Exception as e:
+        results = {
+            "error": "Unknown Error",
+            "description": str(e),
+        }
 
     time_taken = time() - start_time
 
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
 
-    result: dict = locals()[function_name]
-    result['time_taken'] = f"{time_taken:.5f}s"
-    result['memory_used'] = f"{peak / 10**6:.1f}MB"
+    results["time_taken"] = f"{time_taken:.5f}s"
+    results["memory_used"] = f"{peak / 10**6:.1f}MB"
 
-    return result
+    return results
 
 
 def validate_monomials() -> bool | list:
@@ -56,13 +66,13 @@ def validate_monomials() -> bool | list:
     :return: Whether the monomials are valid
     """
 
-    monomials = request.get_json()['monomials']
-    if not monomials['terms']:
+    monomials = request.get_json()["monomials"]
+    if not monomials["terms"]:
         return False
 
     # Use sympy to validate the monomials
     terms = []
-    for monomial in monomials['terms']:
+    for monomial in monomials["terms"]:
         print(monomial)
         try:
             terms.append(sp.sympify(monomial))
@@ -72,39 +82,46 @@ def validate_monomials() -> bool | list:
     # Get the x terms, and check if they are in the correct format
     # i.e. x1 to xn, where n is the number of dimensions
     # e.g. x1**2; x2 + 2 is valid if dimensions = 2
-    dimensions = monomials['dimensions']
+    dimensions = monomials["dimensions"]
     for term in terms:
         for x in term.atoms(sp.Symbol):
-            if not x.name.startswith('x') or not x.name[1:].isdigit() or int(x.name[1:]) > dimensions:
+            if (
+                not x.name.startswith("x")
+                or not x.name[1:].isdigit()
+                or int(x.name[1:]) > dimensions
+            ):
                 return False
 
     return monomials
 
 
-@bp.route('/', endpoint='index', methods=['GET', 'POST'])
+@bp.route("/", endpoint="index", methods=["GET", "POST"])
 def index():
     # TODO: use enums for easier refactoring
     models = [
-        {'title': "Linear", 'description': ""},
-        {'title': "Non-Linear Polynomial", 'description': ""},
+        {"title": "Linear", "description": ""},
+        {"title": "Non-Linear Polynomial", "description": ""},
     ]
 
     timings = [
-        {'title': "Discrete-Time", 'description': ""},
-        {'title': "Continuous-Time", 'description': ""},
+        {"title": "Discrete-Time", "description": ""},
+        {"title": "Continuous-Time", "description": ""},
     ]
 
     modes = [
-        {'title': "Stability", 'description': ""},
-        {'title': "Safety", 'description': ""},
-        {'title': "Reachability", 'description': "", 'disabled': True},
-        {'title': "Reach and Avoid", 'description': "", 'disabled': True},
+        {"title": "Stability", "description": ""},
+        {"title": "Safety", "description": ""},
+        {"title": "Reachability", "description": "", "disabled": True},
+        {"title": "Reach and Avoid", "description": "", "disabled": True},
     ]
 
-    return render_inertia('Dashboard', {
-        'models': models,
-        'timings': timings,
-        'modes': modes,
-        'monomials': lazy_include(validate_monomials),
-        'result': lazy_include(calculate_result),
-    })
+    return render_inertia(
+        "Dashboard",
+        {
+            "models": models,
+            "timings": timings,
+            "modes": modes,
+            "monomials": lazy_include(validate_monomials),
+            "result": lazy_include(calculate_result),
+        },
+    )
