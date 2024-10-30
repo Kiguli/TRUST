671afef4aa062c69abe2c04869d5c34b08076168
Jamie Gardner <jamie@tgo.dev>
2024-10-30 09:48:33 +0000

    Start adding results files

--- vite/js/Components/Atoms/Input.vue
+++ vite/js/Components/Atoms/Input.vue
@@ -4,21 +4,18 @@ defineProps({
         type: Boolean,
         default: false,
     },
-})
+});
 
 const model = defineModel();
-
 </script>
 
 <template>
     <input
         v-model="model"
         :class="[
-            hasError ? 'ring-2 ring-inset ring-red-600 dark:ring-2 focus:ring-red-600' : '',
-            'block h-10 w-full min-w-0 flex-1 rounded-r-md border-0 px-2 py-1.5',
-            'text-sm leading-6 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 ',
-            'placeholder:text-gray-400',
-            'focus:ring-2 focus:ring-inset focus:ring-purple-300',
-            'dark:border-none dark:bg-gray-950 dark:text-gray-100 dark:outline-none dark:ring-0 disabled:opacity-30'
-        ]" />
+            hasError
+                ? 'ring-2 ring-inset ring-red-600 focus:ring-red-600 dark:ring-2'
+                : '',
+        ]"
+        class="block h-10 w-full min-w-0 rounded-r-md border-0 px-2 py-1.5 text-sm leading-6 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-300 disabled:opacity-30 dark:border-none dark:bg-gray-950 dark:text-gray-100 dark:outline-none dark:ring-0" />
 </template>
