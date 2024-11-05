48e03a38f03601ace073d7993f8e58bc5d325cbf
Jamie Gardner <jamie@tgo.dev>
2024-11-05 13:29:17 +0000

    wip: matrix input

---
+++ vite/js/Components/Organisms/MatrixInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    dimension: {
+        type: Number,
+        required: true
+    },
+    monomials: {
+        type: Array,
+        required: true
+    },
+})
+
+const N = computed(() => props.monomials.length)
+
+const model = defineModel()
+</script>
+
+<template>
+    <slot name="title"></slot>
+    <slot name="description"></slot>
+</template>
+
+<style scoped></style>
