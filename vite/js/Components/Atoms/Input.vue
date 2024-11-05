3f2b9f7c4796735a32371ce32bb8639aaa7a7f6d
Jamie Gardner <jamie@tgo.dev>
2024-11-05 13:57:34 +0000

    Light v dark parity

--- vite/js/Components/Atoms/Input.vue
+++ vite/js/Components/Atoms/Input.vue
@@ -17,5 +17,5 @@ const model = defineModel();
                 ? 'ring-2 ring-inset ring-red-600 focus:ring-red-600 dark:ring-2'
                 : '',
         ]"
-        class="block h-10 w-full min-w-0 rounded-r-md border-0 px-2 py-1.5 text-sm leading-6 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-300 disabled:opacity-30 dark:border-none dark:bg-gray-950 dark:text-gray-100 dark:outline-none dark:ring-0" />
+        class="block h-10 w-full min-w-0 rounded-r-md border-0 px-2 py-1.5 text-sm leading-6 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-300 disabled:opacity-30 dark:border-none dark:bg-gray-950 dark:text-gray-100 dark:outline-none dark:ring-0 dark:placeholder:text-gray-500" />
 </template>
